6e8d0bb7a14b6b51be41f964727b4a1281fa286e
John Caesar Patac <jc@bposeats.com>
2020-01-24 16:01:13 +0800

    Added capture image skeleton

--- src/index.js
+++ src/index.js
@@ -2,21 +2,34 @@ import * as faceapi from 'face-api.js';
 
 let video = document.getElementById('video');
 let startButton = document.getElementById('startBtn');
-let labels = [];
+let enterData = document.getElementById('enterData');
+let labeledFaceDescriptors;
+let labels = ['caesar'];
 
-Promise.all([
-    faceapi.loadSsdMobilenetv1Model('./models'),
-    faceapi.loadFaceLandmarkModel('./models'),
-    faceapi.loadFaceRecognitionModel('./models'),
-    faceapi.loadFaceExpressionModel('./models')
-]).then(setupVideo)
-
-video.addEventListener('play', () => {
-    detectFace();
+document.addEventListener('DOMContentLoaded', function(event) {
+    setupVideo();
+    Promise.all([
+        faceapi.loadSsdMobilenetv1Model('./models'),
+        faceapi.loadFaceLandmarkModel('./models'),
+        faceapi.loadFaceRecognitionModel('./models'),
+        faceapi.loadFaceExpressionModel('./models')
+    ]).then(() => {
+        console.log('Models loaded!');
+        startButton.disabled = false;
+        // enterData.disabled = false;
+    })
 })
 
-startButton.addEventListener('click', () => {
+enterData.addEventListener('click', () => {
     video.play();
+    takeSnap();
+})
+
+// video.addEventListener('play', () => {
+// })
+
+startButton.addEventListener('click', () => {
+    setupReference();
 })
 
 function setupVideo() {
@@ -25,18 +38,47 @@ function setupVideo() {
         stream => video.srcObject = stream,
         err => console.error(err)
     )
-    setupReference();
 }
 
-let labeledFaceDescriptors;
+function takeSnap() {
+    let intervalCount = 0;
+    let snapTimer = setInterval(() => {
+        let detectFace = faceapi.detectSingleFace(video);
+        if (detectFace) {
+            intervalCount++;
+            console.log(intervalCount);
+            let canvas = document.createElement('canvas');
+            let ctx = canvas.getContext('2d');
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            ctx.drawImage(video, 0, 0);
+            downloadImage(canvas, intervalCount)
+            if (intervalCount >= 5) {
+                console.log('Done!');
+                clearInterval(snapTimer);
+            }
+        } else {
+            intervalCount = 0;
+        }
+    }, 1000 * 3)
+}
+
+function downloadImage(canvas, count) {
+    let downloadLink = document.createElement('a');
+    downloadLink.setAttribute('download', `caesar_${count}.jpg`);
+    let dataURL = canvas.toDataURL('image/jpeg');
+    let url = dataURL.replace(/^data:image\/jpg/,'data:application/octet-stream');
+    downloadLink.setAttribute('href', url);
+    downloadLink.click();
+}
 
 async function setupReference() {
-    if (labels.size > 1) {
+    if (labels.length > 0) {
         labeledFaceDescriptors = await Promise.all(
             labels.map(async label => {
-                let imgUrl = `./reference_images/${label}.jpg`;
+                let imgUrl = `./reference_images/${label}_1.jpg`;
                 let faceDescriptors = [];
-                let img = await faceapi.fetchImage(imgUr);
+                let img = await faceapi.fetchImage(imgUrl);
                 let fullFaceDescription = await faceapi.detectSingleFace(img).
                                                         withFaceLandmarks().
                                                         withFaceDescriptor();
@@ -44,10 +86,11 @@ async function setupReference() {
                     throw new Error(`No face detected for ${label}`);
                 }
                 faceDescriptors.push(fullFaceDescription.descriptor)
-                startButton.disabled = false;
+                video.play();
                 return new faceapi.LabeledFaceDescriptors(label, faceDescriptors);
             })
         )
+        detectFace();
     } else {
         alert('No data available')
     }
@@ -68,7 +111,7 @@ function detectFace() {
         let ctx = canvas.getContext('2d');
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        let maxDescriptorDistance = 0.6;
+        let maxDescriptorDistance = 0.55;
         let faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance);
         let results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor));
 
